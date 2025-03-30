8ab22ffd36ff26d1b9ef40d3fb923e13d78160cb
Joeatable <yuqiao.24@intl.zju.edu.cn>
2025-03-31 00:17:31 +0800

    variables.py

--- practical 4/variables.py
+++ practical 4/variables.py
@@ -1,9 +1,9 @@
 a = 15 #walk to bus station
 b = 75 #bus time
-c = a + b #sum of time
+c = a + b #T=total time for bus option
 d = 90 #driving time
 e = 5 #walk to the office
-f = d + e #sum of time
+f = d + e #total time for car option
 if c < f :
     print ("by bus")
 elif c == f:
@@ -11,4 +11,16 @@ elif c == f:
 else:
     print("by car") 
 
-# bus is faster(:
+# bus is faster
+
+#Boolean variable setup 
+X = True
+Y = False
+W = X and Y
+
+# Truth table for W:
+#   X   ｜   Y   ｜ W = X+Y |
+#  True     True     True   
+#  True     False    False 
+#  False    True     False
+#  False    False    False 
